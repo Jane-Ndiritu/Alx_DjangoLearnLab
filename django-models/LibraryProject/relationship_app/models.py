af7e2c3f05889b7c9b1f3342c1a70f39f7af5321
jane <jayndiritu36@gmail.com>
2025-11-16 09:35:01 +0300

    Implementing User Authentication in Django

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -8,7 +8,7 @@ class Author(models.Model):
     def __str__(self):
         return self.name
 class Book(models.Model):
-    title = models.Cherfield(max_length=200)
+     title = models.Cherfield(max_length=200)
     author = models.ForeignKey(
         Author,
         on_delete=models.CASCADE,
