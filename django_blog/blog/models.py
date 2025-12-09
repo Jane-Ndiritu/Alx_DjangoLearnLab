64fd50519ff0a82bde25188732fa7d98f42c8317
jane <jayndiritu36@gmail.com>
2025-12-09 16:19:31 +0300

    intergrated tagging functions

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,15 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import User 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=100, unique=True)
 
+    def __str__(self):
+        return self.name
+    
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
 author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+tags = models.ManyToManyField(Tag, related_name='posts', blank=True)
 
 def __str__(self):
         return self.title
+
 class Comment(models.Model):
     post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
