0f76f2e19837284be3d9d13d150aa21c0e256638
jane <jayndiritu36@gmail.com>
2025-12-10 12:33:01 +0300

    corrected the structure in serializer and imported tokens and user model

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -13,7 +13,7 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
         fields = ['username', 'email', 'password', 'bio', 'profile_picture']
 
     def create(self, validated_data):
-        user = CustomUser.objects.create_user(
+        user = get_user_model().objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
             password=validated_data['password'],
@@ -21,7 +21,7 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
             profile_picture=validated_data.get('profile_picture')
         )
 
-        # 🔥 Create authentication token automatically
+        # Create authentication token automatically
         Token.objects.create(user=user)
 
         return user
