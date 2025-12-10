7a7037e6ce2fdf7127f709efde1af7f28eb3e02d
jane <jayndiritu36@gmail.com>
2025-12-10 19:00:06 +0300

    updated views post

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions, status
+from rest_framework.response import Response
+from django.db.models import Q
+from .models import Post
+from .serializers import PostSerializer
 
-# Create your views here.
+class FeedView(generics.ListAPIView):
+    """
+    View to show posts from users that the current user follows
+    """
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated] 
+    
+    def get_queryset(self):
+        user = self.request.user
+       
+        following_users = user.following.all()  
+        queryset = Post.objects.filter(
+            author__in=following_users
+        ).order_by('-created_at')  
+        
+        return queryset
