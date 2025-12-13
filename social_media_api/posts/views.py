2628f2b38c7041ed70bb06e0fd692ec2e9b23098
jane <jayndiritu36@gmail.com>
2025-12-13 12:58:23 +0300

    changes the 404 import

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,4 +1,4 @@
-from rest_framework.generics import get_object_or_404
+from rest_framework.generics import generics.get_object_or_404
 from rest_framework import generics, permissions, viewsets, status
 from rest_framework.response import Response
 from rest_framework.decorators import action
